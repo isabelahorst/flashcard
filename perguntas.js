41b7c3c5d743edb7d9b90c54359745175c9ba3f1
isabelahorst <horst.isabela@escola.pr.gov.br>
2025-08-11 09:01:54 -0300

    hkjkjkj

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao(
+    'Educação Fisica',
+    'No vôlei, como se vhama o movento que inicia o jogo?',
+    'Saque'
+)
+criaCartao(
+    'Matemática',
+    'Quantos é 5+6?',
+    'A resposta certa é 11'
+)
+criaCartao(
+    'Calendário',
+    'Que dia é vespéra de natal?'
+    'Dia 24 de dezembro'
+)
+criaCartao(
+    'Geografia',
+    'Onde fica localizado Santa Catarina?',
+    'No sul do Brasil'
+)
+criaCartao(
+    'Biologia',
+    'Qual parte da célula controla a entra e a saida de coisas?',
+    'Membrana plásmatica'
+)
+criaCartao(
+    'Quimica',
+    'Qual gás os seres humanos usam para respirar?',
+     'Gás oxigênio'
+)
